7d07720ed33db68af801a72265924c503d60c8a9
Gracy Costa <gracywl@gmail.com>
2010-11-23 16:40:20 -0300

    Segundo Commit

---
+++ src/teste/NovaClasse.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teste;
+
+/**
+ *
+ * @author Gracy
+ */
+public class NovaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("SAlvando uma nova classe");
+        System.out.println("Alteração");
+    }
+}
